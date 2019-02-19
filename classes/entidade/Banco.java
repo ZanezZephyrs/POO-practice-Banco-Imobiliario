7ca1ba19571e5be96f3b67e090bf8d78efc69996
GiKerche <45881742+GiKerche@users.noreply.github.com>
2019-02-19 09:19:06 -0300

    Update Banco.java

--- classes/entidade/Banco.java
+++ classes/entidade/Banco.java
@@ -10,8 +10,27 @@ package entidade;
  * @author Giovana
  */
 public class Banco {
-    
+    private int id_jogador;
+    private int num_casas;
+    private int num_hoteis;
 }
 
-public Banco(){
+/*Construtor da classe Jogador com 3 parâmetros*/
+public Banco(int id_jogador, int num_casas, int num_hoteis) {
+		this.id_jogador = 0; /*Primeiro jogador*/
+		this.num_casas = 32;
+		this.num_hoteis = 12;
 }
+
+public int getID_jogador(){
+	return id_jogador;
+}
+
+public int getNum_casas() {
+	return num_casas;
+}
+
+public int getNum_hoteis(){
+	return num_hoteis;
+}
+
